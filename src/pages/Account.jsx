5e8d1c9eead3455e09500e090181fc64a37c1925
Steve Lockhart <steve.l@signa-consultants.com>
2025-11-08 12:26:19 +0000

    File changes

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -863,7 +863,7 @@ export default function Account() {
                     padding: '16px',
                     backgroundColor: colors.fieldBg,
                     borderRadius: '12px',
-                    borderLeft: '4px solid '#1A1D1F''
+                    borderLeft: '4px solid #1A1D1F'
                   }}>
                     <div className="flex items-center gap-3">
                       <div style={{
