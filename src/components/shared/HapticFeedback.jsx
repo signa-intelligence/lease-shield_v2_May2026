6e27b3ffd279271ad78f750d0e00e7bd40a742c4
Steve Lockhart <steve.l@signa-consultants.com>
2025-11-18 01:40:05 +0000

    File changes

--- src/components/shared/HapticFeedback.jsx
+++ src/components/shared/HapticFeedback.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 /**
  * Haptic Feedback Utility
  * Provides tactile feedback on PWA/mobile devices
@@ -5,44 +7,68 @@
 
 export const haptic = {
   light: () => {
-    if ('vibrate' in navigator) {
-      navigator.vibrate(10);
+    try {
+      if ('vibrate' in navigator) {
+        navigator.vibrate(10);
+      }
+    } catch (err) {
+      // Silently fail - haptic is optional
     }
   },
   
   medium: () => {
-    if ('vibrate' in navigator) {
-      navigator.vibrate(20);
+    try {
+      if ('vibrate' in navigator) {
+        navigator.vibrate(20);
+      }
+    } catch (err) {
+      // Silently fail
     }
   },
   
   heavy: () => {
-    if ('vibrate' in navigator) {
-      navigator.vibrate([30, 10, 30]);
+    try {
+      if ('vibrate' in navigator) {
+        navigator.vibrate([30, 10, 30]);
+      }
+    } catch (err) {
+      // Silently fail
     }
   },
   
   success: () => {
-    if ('vibrate' in navigator) {
-      navigator.vibrate([10, 50, 10]);
+    try {
+      if ('vibrate' in navigator) {
+        navigator.vibrate([10, 50, 10]);
+      }
+    } catch (err) {
+      // Silently fail
     }
   },
   
   error: () => {
-    if ('vibrate' in navigator) {
-      navigator.vibrate([50, 50, 50]);
+    try {
+      if ('vibrate' in navigator) {
+        navigator.vibrate([50, 50, 50]);
+      }
+    } catch (err) {
+      // Silently fail
     }
   },
   
   // Wrapper for buttons
   tap: (intensity = 'light') => {
-    if ('vibrate' in navigator) {
-      switch(intensity) {
-        case 'light': navigator.vibrate(10); break;
-        case 'medium': navigator.vibrate(20); break;
-        case 'heavy': navigator.vibrate(30); break;
-        default: navigator.vibrate(10);
+    try {
+      if ('vibrate' in navigator) {
+        switch(intensity) {
+          case 'light': navigator.vibrate(10); break;
+          case 'medium': navigator.vibrate(20); break;
+          case 'heavy': navigator.vibrate(30); break;
+          default: navigator.vibrate(10);
+        }
       }
+    } catch (err) {
+      // Silently fail
     }
   }
 };
@@ -53,7 +79,11 @@ export const haptic = {
 export const withHaptic = (Component, intensity = 'light') => {
   return (props) => {
     const handleClick = (e) => {
-      haptic.tap(intensity);
+      try {
+        haptic.tap(intensity);
+      } catch (err) {
+        // Silently fail
+      }
       if (props.onClick) {
         props.onClick(e);
       }
