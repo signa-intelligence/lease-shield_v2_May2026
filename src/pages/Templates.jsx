2fa04753016cd4ad2886f3961d38c01d55c8f330
Steve Lockhart <steve.l@signa-consultants.com>
2025-12-09 01:03:11 +0000

    File changes

--- src/pages/Templates.jsx
+++ src/pages/Templates.jsx
@@ -1006,7 +1006,7 @@ export default function Templates() {
           </div>
           
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
-            {preSigningTemplates.map((template) => renderTemplateCard(template, !template.id))}
+            {[...TEMPLATES.filter(tmpl => tmpl.preSigning), ...(customTemplates || []).filter(tmpl => tmpl.category === 'pre_signing')].map((template) => renderTemplateCard(template, !template.id))}
           </div>
         </div>
 
