713f4fb10bd1940872e848768a945d8407a1cd90
Steve Lockhart <steve.l@signa-consultants.com>
2025-12-09 04:55:00 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -28,6 +28,7 @@ import RevenueAnalytics from './pages/RevenueAnalytics';
 import AdminTemplates from './pages/AdminTemplates';
 import RecycleBin from './pages/RecycleBin';
 import Welcome from './pages/Welcome';
+import CookieSync from './pages/CookieSync';
 import __Layout from './Layout.jsx';
 
 
@@ -62,6 +63,7 @@ export const PAGES = {
     "AdminTemplates": AdminTemplates,
     "RecycleBin": RecycleBin,
     "Welcome": Welcome,
+    "CookieSync": CookieSync,
 }
 
 export const pagesConfig = {
