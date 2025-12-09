ed97214d80f6505f8f29f14ac7e7d88435a1014a
Steve Lockhart <steve.l@signa-consultants.com>
2025-12-09 05:47:36 +0000

    File changes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -10,8 +10,9 @@ import { createPageUrl } from "@/utils";
 import DebouncedSearch from "../components/shared/DebouncedSearch";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import SkeletonLoader from "../components/shared/SkeletonLoader";
+import AuthGuard from "../components/shared/AuthGuard";
 
-export default function Search() {
+function SearchContent() {
   const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState("");
   const [filterType, setFilterType] = useState("all");
