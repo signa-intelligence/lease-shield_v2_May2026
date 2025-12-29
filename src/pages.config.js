571fbc2be476d78f6a142c432ab4948ac291cd32
Steve Lockhart <steve.l@signa-consultants.com>
2025-12-29 06:05:15 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -9,7 +9,6 @@ import Analytics from './pages/Analytics';
 import CaseDetails from './pages/CaseDetails';
 import Cases from './pages/Cases';
 import CookieSync from './pages/CookieSync';
-import Dashboard from './pages/Dashboard';
 import DepositTracker from './pages/DepositTracker';
 import DocumentVault from './pages/DocumentVault';
 import Documents from './pages/Documents';
@@ -41,6 +40,7 @@ import Timeline from './pages/Timeline';
 import UploadScan from './pages/UploadScan';
 import Welcome from './pages/Welcome';
 import cookieSync from './pages/cookie-sync';
+import Dashboard from './pages/Dashboard';
 import __Layout from './Layout.jsx';
 
 
@@ -56,7 +56,6 @@ export const PAGES = {
     "CaseDetails": CaseDetails,
     "Cases": Cases,
     "CookieSync": CookieSync,
-    "Dashboard": Dashboard,
     "DepositTracker": DepositTracker,
     "DocumentVault": DocumentVault,
     "Documents": Documents,
@@ -88,6 +87,7 @@ export const PAGES = {
     "UploadScan": UploadScan,
     "Welcome": Welcome,
     "cookie-sync": cookieSync,
+    "Dashboard": Dashboard,
 }
 
 export const pagesConfig = {
