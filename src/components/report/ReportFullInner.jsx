90f57b5d6e335077ef78a7e921af6a51f2dd48ea
Steve Lockhart <steve.l@signa-consultants.com>
2026-01-27 03:08:42 +0000

    Manual code change

--- src/components/report/ReportFullInner.jsx
+++ src/components/report/ReportFullInner.jsx
@@ -546,7 +546,7 @@ export default function ReportFullInner({ scanId, leaseId, showDebug, forensicDa
 
         // STEP 2: lease
         logStep("FETCH_LEASE_START", { leaseId });
-        const leaseArr = await base44.entities.Lease.filter({});
+        const leaseArr = await base44.entities.Lease.filter({ owner_email: userRes.email });
         const leaseData = leaseArr?.find(l => l.id === leaseId) || null;
         logStep("FETCH_LEASE_COMPLETE", { found: !!leaseData });
 
